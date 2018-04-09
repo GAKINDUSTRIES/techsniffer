27726af1f5375bf1964c058aea36d74154540114
Guillermo Kuster <hiperguille10@gmail.com>
2018-04-08 23:01:59 -0300

    Set structure of the project, add gems needed and prepare test enviroment with useful tools

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: '/api/v1/users', controllers: {
+    registrations:  'api/v1/registrations',
+    sessions:  'api/v1/sessions',
+    passwords:  'api/v1/passwords'
+  }
+
+  namespace :api do
+    namespace :v1, defaults: { format: :json } do
+      devise_scope :user do
+        get :status, to: 'api#status'
+        resources :users, only: :show
+        resource :user, only: :update do
+          get :profile
+          controller :sessions do
+            post :facebook, on: :collection
+          end
+        end
+      end
+    end
+  end
+end
