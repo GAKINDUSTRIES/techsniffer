eb58293820f2a417a33ad371cc5c4db7c5d178b1
Guillermo Kuster <hiperguille10@gmail.com>
2018-05-15 22:02:15 -0300

    add stats

--- app/decorators/admin_decorator.rb
+++ app/decorators/admin_decorator.rb
@@ -6,4 +6,11 @@ class AdminDecorator < Draper::Decorator
   def full_name
    ' ' + first_name + ' ' + last_name + '.'
   end
+
+  def cups_of_coffe
+    initial_amount = 1500
+    amount_per_day = 2
+    days_left = (Date.today - created_at.to_date).to_i
+    initial_amount + amount_per_day * days_left
+  end
 end
