468c31a5413e54cb102096c1279d4d63edaf8928
Guillermo Kuster <hiperguille10@gmail.com>
2018-05-19 21:47:36 -0300

    add assets (javascript libraries, fonts, images and base stylesheets)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,11 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery.js
 //= require jquery_ujs
+//= require plugins
+//= require axios.js
+//= require main.js
+//= require modernizr.js
+//= require pace.min.js
 //= require_tree .
