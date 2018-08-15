2ea5f21bd615a99a88ae382846df136f0684d50b
Guillermo Kuster <hiperguille10@gmail.com>
2018-08-15 20:12:45 -0300

    add default avatar for recommenders without image

--- app/decorators/recommendation_decorator.rb
+++ app/decorators/recommendation_decorator.rb
@@ -1,6 +1,19 @@
 class RecommendationDecorator < Draper::Decorator
   delegate_all
 
+  def display_avatar
+    if recommender_avatar.present?
+      h.image_tag(recommender_avatar, class: 'testimonials__avatar')
+    else
+      h.render partial: 'shared/avatar', locals: { initials: initials }
+    end
+  end
+
+  def initials
+    return if recommender_name.nil?
+    recommender_name.split.map(&:first).join
+  end
+
   def recommender_postion_and_role
     role = recommender_role.upcase
     return role if recommender_company.blank?
