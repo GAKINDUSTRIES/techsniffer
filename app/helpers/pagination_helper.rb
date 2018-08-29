e4c829b252739c47e07193ba91c220e841955e49
Guillermo Kuster <hiperguille10@gmail.com>
2018-08-28 21:59:26 -0300

    fix issue with pagination of articles when page was not passed as a param

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -1,12 +1,14 @@
 module PaginationHelper
   delegate :url_helpers, to: 'Rails.application.routes'
 
-  def next_page(current_page, per_page, total)
+  def next_page(current_page= 1, per_page, total)
+    current_page = 1 if current_page.zero? || current_page.blank?
     total_amount_of_pages = (total.to_f / per_page.to_f).ceil
     (total_amount_of_pages.to_i > current_page.to_i) ? true : false
   end
 
   def previous_page(current_page, per_page, total)
+    current_page = 1 if current_page.zero? || current_page.blank?
     current_page.to_i > 1 ? true : false
   end
 end
