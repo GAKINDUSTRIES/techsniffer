5d406275d1e09599f6c9e7d5eabf5b16e619c036
Guillermo Kuster <hiperguille10@gmail.com>
2019-05-09 21:42:59 -0300

    Update web copy

--- app/decorators/admin_decorator.rb
+++ app/decorators/admin_decorator.rb
@@ -4,7 +4,7 @@ class AdminDecorator < Draper::Decorator
   delegate_all
 
   def full_name
-    ' ' + first_name + ' ' + last_name + '.'
+    ' ' + first_name + ' ' + last_name + ','
   end
 
   def cups_of_coffe
